021245dcbb270804c146a439509ac4f3a0b9dc58
Pecika <peca.ognjenovic21@gmail.com>
2023-08-09 18:20:16 +0200

    dodate rute i popravljen css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Navbar from './komponente/Navbar';
+import Pocetna from './komponente/Pocetna';
+import Zadaci from './komponente/Zadaci';
+import KreirajZadatak from './komponente/KreirajZadatak';
+import HitnostZadatka from './komponente/HitnostZadatka'; // Dodajemo HitnostZadatka komponentu
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import FiltriranjeZadataka from './komponente/FiltriranjeZadataka';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+      setTasks(JSON.parse(savedTasks));
+    }
+  }, []);
+
+  const addTask = (newTask) => {
+    const updatedTasks = [...tasks, newTask];
+    setTasks(updatedTasks);
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>        
+        </header>
+
+        <body>
+          <nav>
+            <Navbar />
+          </nav>
+
+          <Routes>
+            <Route exact path="/" element={<Pocetna />} />
+            <Route path="/to-do-lista" element={
+              <div>
+              
+                <Zadaci tasks={tasks} />
+              </div>
+            } />
+            <Route path="/kreiraj-zadatak" element={<KreirajZadatak addTask={addTask} />} />
+          </Routes>
+        </body>
+      </div>
+    </Router>
   );
 }
 
